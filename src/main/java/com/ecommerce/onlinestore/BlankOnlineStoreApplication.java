a7e27149e72b5024e6ac276f2a3ad4764e32e1f2
Alvin <alvinnguyen58@gmail.com>
2023-08-21 23:35:56 -0700

    Initial Commit

---
+++ src/main/java/com/ecommerce/onlinestore/BlankOnlineStoreApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.onlinestore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlankOnlineStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlankOnlineStoreApplication.class, args);
+	}
+
+}
